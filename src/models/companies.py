0bac52fb9756f9924f1c439949761071f32e6b3d
TheADLAN <probexanomaly@gmail.com>
2022-09-17 03:41:29 +0800

    added more models

--- src/models/companies.py
+++ src/models/companies.py
@@ -1,1 +1,28 @@
 # -- coding: utf-8 --
+
+from datetime import date, datetime
+from sqlalchemy import (
+    String,
+    Integer,
+    Column,
+    DateTime,
+    Boolean, 
+    ForeignKey,      
+)
+from src.models.db.base_class import Base
+
+
+class Companies(Base):
+    __tablename__="companies"
+    id = Column(Integer, primary_key=True, index=True)
+    company_name = Column(String(255), nullable=False, index=True)
+    email = Column(String(255), unique=True, nullable=False, index=True)
+    company_phone_no = Column(Integer, nullable=False, index=True)
+    address = Column(String(255), nullable=False, unique=True, index=True)
+
+    #soft delete
+    created_at = Column(DateTime, default=datetime.now())
+    updated_at = Column(DateTime, default=datetime.now())
+    deleted = Column(Boolean, default=False)
+
+
---
+++ src/models/db/base.py
@@ -0,0 +1,3 @@
+from src.models.db.base_class import Base
+from src.models.companies import Companies
+from src.models.people import People
